dc37076830842a60f04e28cfd0398d7631c134d7
JeanPablo <JeanPablo@192.168.1.2>
2015-05-01 18:11:25 -0300

    Turma

---
+++ SistemaAcademico/src/model/pojo/Turma.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.pojo;
+
+/**
+ *
+ * @author JeanPablo
+ */
+public class Turma {
+    private Integer ano;
+    private Integer periodo;
+    private String local;
+    private String horario;
+    private Integer numeroDeVagas;
+}
