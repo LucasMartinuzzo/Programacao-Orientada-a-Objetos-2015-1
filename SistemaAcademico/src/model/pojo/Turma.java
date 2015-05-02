716d000af101fe70f5b34ed5152799c542e431ed
JeanPablo <JeanPablo@192.168.1.2>
2015-05-01 22:57:32 -0300

    Turma

--- SistemaAcademico/src/model/pojo/Turma.java
+++ SistemaAcademico/src/model/pojo/Turma.java
@@ -22,4 +22,60 @@ public class Turma {
     private List<Aluno> aluno;
     private List<Atividade> atividade;
     private List<Falta> falta;
+    
+    public Turma (Integer ano, Integer periodo, String local, String horario, Integer numeroDeVagas,
+            Disciplina disciplina, Professor professor, List<Aluno> aluno, List <Atividade> atividade) {
+        this.ano = ano;
+        this.periodo = periodo;
+        this.local = local;
+        this.horario = horario;
+        this.numeroDeVagas = numeroDeVagas;
+        this.disciplina = disciplina;
+        this.professor = professor;
+        this.atividade = atividade;
+    }
+    
+    public Integer getAno () {
+        return this.ano;
+    }
+    
+    //public void setAno (Integer ano) {
+    //    this.ano = ano;
+    //}
+    
+    public Integer getPeriodo () {
+        return this.periodo;
+    }
+    
+    public String getLocal () {
+        return this.local;
+    }
+    
+    public String getHorario () {
+        return this.horario;
+    }
+    
+    public Integer getNumeroDeVagas () {
+        return this.numeroDeVagas;
+    }
+    
+    public Disciplina getDisciplina () {
+        return this.disciplina;
+    }
+    
+    public Professor getProfessor () {
+        return this.professor;
+    }
+    
+    public List<Aluno> getListaDeAlunos () {
+        return this.aluno;
+    }
+    
+    public List<Atividade> getListaDeAtividades () {
+        return this.atividade;
+    }
+    
+    public List<Falta> getListaDeFaltas () {
+        return this.falta;
+    }
 }
