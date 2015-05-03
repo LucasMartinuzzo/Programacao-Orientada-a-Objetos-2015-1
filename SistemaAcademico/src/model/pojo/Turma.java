5e1a1d2900bc9ffdbce71985a93ff88cd0d5aab1
JeanPablo <JeanPablo@192.168.1.2>
2015-05-03 17:42:22 -0300

    Turma

--- SistemaAcademico/src/model/pojo/Turma.java
+++ SistemaAcademico/src/model/pojo/Turma.java
@@ -14,68 +14,101 @@ import java.util.List;
 public class Turma {
     private Integer ano;
     private Integer periodo;
-    private String local;
-    private String horario;
     private Integer numeroDeVagas;
     private Disciplina disciplina;
     private Professor professor;
+    private List<Horario> horario;
     private List<Aluno> aluno;
     private List<Atividade> atividade;
-    private List<Falta> falta;
     
-    public Turma (Integer ano, Integer periodo, String local, String horario, Integer numeroDeVagas,
-            Disciplina disciplina, Professor professor, List<Aluno> aluno, List <Atividade> atividade) {
+    public Turma (Integer ano, Integer periodo, String local,
+            Integer numeroDeVagas, Disciplina disciplina, Professor professor,
+            List<Horario> horario) {
         this.ano = ano;
         this.periodo = periodo;
-        this.local = local;
-        this.horario = horario;
         this.numeroDeVagas = numeroDeVagas;
         this.disciplina = disciplina;
         this.professor = professor;
-        this.atividade = atividade;
+        this.horario = horario;
     }
     
     public Integer getAno () {
         return this.ano;
     }
     
-    //public void setAno (Integer ano) {
-    //    this.ano = ano;
-    //}
+    public void setAno (Integer ano) {
+        this.ano = ano;
+    }
     
     public Integer getPeriodo () {
         return this.periodo;
     }
     
-    public String getLocal () {
-        return this.local;
-    }
-    
-    public String getHorario () {
-        return this.horario;
+    public void setPeriodo (Integer periodo) {
+        this.periodo = periodo;
     }
     
     public Integer getNumeroDeVagas () {
         return this.numeroDeVagas;
     }
     
+    public void setNumeroDeVagas (Integer numeroDeVagas) {
+        this.numeroDeVagas = numeroDeVagas;
+    }
+    
     public Disciplina getDisciplina () {
         return this.disciplina;
+    }
+    
+    public void setDisciplina (Disciplina disciplina) {
+        this.disciplina = disciplina;
     }
     
     public Professor getProfessor () {
         return this.professor;
     }
     
+    public void setProfessor (Professor professor) {
+        this.professor = professor;
+    }
+    
+    //****FAZER OS MÉTODOS RELATIVOS AOS HORÁRIOS DE AULA**********
+    
+    //***************** ALUNOS *************************
     public List<Aluno> getListaDeAlunos () {
         return this.aluno;
     }
     
+    public boolean contemAluno (Aluno aluno) {
+        return this.aluno.contains(aluno);
+    }
+    
+    public boolean adicaoDeAlunoValida (Aluno aluno) {
+        return (this.contemAluno(aluno) || this.aluno.size() == this.numeroDeVagas);
+    }
+    
+    public void adicionarAluno (Aluno aluno) {
+        this.aluno.add(aluno);
+    }
+    
+    public void removerAluno (Aluno aluno) {
+        this.aluno.remove(aluno);
+    }
+    
+    //**************** ATIVIDADES *********************
     public List<Atividade> getListaDeAtividades () {
         return this.atividade;
     }
     
-    public List<Falta> getListaDeFaltas () {
-        return this.falta;
+    public boolean contemAtividade (Atividade atividade) {
+        return this.atividade.contains(atividade);
+    }
+    
+    public void adicionarAtividade (Atividade atividade) {
+        this.atividade.add(atividade);
+    }
+    
+    public void removerAtividade (Atividade atividade) {
+        this.atividade.remove(atividade);
     }
 }
