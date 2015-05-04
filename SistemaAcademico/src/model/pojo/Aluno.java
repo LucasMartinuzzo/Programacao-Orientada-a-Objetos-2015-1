71feec6643017886c1a79151954b6b5db9147fe9
JeanPablo <JeanPablo@192.168.9.223>
2015-05-04 14:12:24 -0300

    Atividade

--- SistemaAcademico/src/model/pojo/Aluno.java
+++ SistemaAcademico/src/model/pojo/Aluno.java
@@ -16,7 +16,37 @@ public class Aluno {
     private List<Turma> turma; 
     private List<Falta> falta;
     private List<Nota> nota;
+
+    public Aluno (String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
     
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Turma> getTurma() {
+        return turma;
+    }
+
+    public List<Falta> getFalta() {
+        return falta;
+    }
+
+    public List<Nota> getNota() {
+        return nota;
+    }
 }
