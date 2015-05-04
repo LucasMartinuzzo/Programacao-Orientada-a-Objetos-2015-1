57a27e70fd87503494a74ea0dee9f6a57938dd98
Pedro <Pedro@192.168.11.165>
2015-05-04 14:00:51 -0300

    Professor

--- SistemaAcademico/src/model/pojo/Professor.java
+++ SistemaAcademico/src/model/pojo/Professor.java
@@ -16,4 +16,34 @@ public class Professor {
     private String cpf;
     private String departamento;
     private List<Disciplina> disciplina;
+    
+    public Professor (String nome, String cpf, String departamento){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.departamento = departamento;
+    }
+    
+    public String getNome (){
+        return nome;
+    }
+    
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+    
+    public String getCpf (){
+        return cpf;
+    }
+    
+    public void setCpf (String cpf){
+        this.cpf = cpf;
+    }
+    
+    public String getDepartamento (){
+        return departamento;
+    }
+    
+    public void setDepartamento (String departamento){
+        this.departamento = departamento;
+    }
 }
