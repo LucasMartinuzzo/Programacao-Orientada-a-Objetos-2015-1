c773d21e82f4020897974ce1ea2dcc4c7a341a2b
JeanPablo <JeanPablo@192.168.1.3>
2015-05-09 14:29:37 -0300

    Atividade  DAO

--- SistemaAcademico/src/model/dao/TurmaDaoImpl.java
+++ SistemaAcademico/src/model/dao/TurmaDaoImpl.java
@@ -5,6 +5,8 @@
  */
 package model.dao;
 
+import java.util.List;
+//import javafx.collections.transformation.SortedList;
 import model.pojo.Aluno;
 import model.pojo.Atividade;
 import model.pojo.Aula;
@@ -16,61 +18,92 @@ import model.pojo.Turma;
  */
 public class TurmaDaoImpl implements TurmaDao {
     
-    private Turma turma;
+    private List<Turma> listaTurma;
     
     @Override
-    public Boolean adicionarAula (Aula aula) {
-        if (this.turma.aula.contains(aula) == false)
-            return this.turma.aula.add(aula);
+    public Boolean salvar (Turma turma) {
+        if (this.contemTurma(turma.getId()) == false)
+           return this.listaTurma.add(turma);
         return false;
     }
     
     @Override
-    public Boolean removerAula (Aula aula) {
-        return this.turma.aula.remove(aula);
+    public Boolean contemTurma (String id) {
+        //ORDENAR LISTA DE TURMAS PELO ID E APLICAR BUSCA BINÁRIA BASEADA NESTE.
+        return false;
     }
     
     @Override
-    public Aula retornaAula (Aula aula) {
-        return this.turma.aula.get(this.turma.aula.indexOf(aula));
+    public Turma obterTurma (String id) {
+        if (this.contemTurma(id))
+            return this.listaTurma.get(this.listaTurma.indexOf(/*?????*/));
+        return null;
     }
     
     @Override
-    public Boolean adicaoDeAlunoValida (Aluno aluno) {
-        return (this.turma.getAluno().contains(aluno) == false || 
-                this.turma.getAluno().size() < this.turma.getNumeroDeVagas());
+    public List<Turma> obterTodas () {
+        return listaTurma;
     }
     
     @Override
-    public Boolean adicionarAluno (Aluno aluno) {
-        if (this.adicaoDeAlunoValida(aluno)) {
-            aluno.getTurma().add(this.turma);
-            return this.turma.getAluno().add(aluno);
+    public Boolean adicionarAula (Turma turma, Aula aula) {
+        if (turma.aula.contains(aula) == false)
+            return turma.aula.add(aula);
+        return false;
+    }
+    
+    @Override
+    public Boolean removerAula (Turma turma, Aula aula) {
+        return turma.aula.remove(aula);
+    }
+    
+    @Override
+    public Aula retornaAula (Turma turma, Aula aula) {
+        if (turma.aula.contains(aula))
+            return turma.aula.get(turma.aula.indexOf(aula));
+        else
+            return null;
+    }
+    
+    @Override
+    public Boolean adicaoDeAlunoValida (Turma turma, Aluno aluno) {
+        return (turma.getAluno().contains(aluno) == false || 
+                turma.getAluno().size() < turma.getNumeroDeVagas());
+    }
+    
+    @Override
+    public Boolean adicionarAluno (Turma turma, Aluno aluno) {
+        if (this.adicaoDeAlunoValida(turma, aluno)) {
+            aluno.getTurma().add(turma);
+            return turma.getAluno().add(aluno);
         }
         return false;
     }
     
     @Override
-    public Boolean removerAluno (Aluno aluno) {
-        if (this.turma.getAluno().remove(aluno))
-            return aluno.getTurma().remove(this.turma);
+    public Boolean removerAluno (Turma turma, Aluno aluno) {
+        if (turma.getAluno().remove(aluno))
+            return aluno.getTurma().remove(turma);
         return false;
     }
     
     @Override
-    public Boolean adicionarAtividade (Atividade atividade) {
-        if (this.turma.getAtividade().contains(atividade) == false)
-            return this.turma.getAtividade().add(atividade);
+    public Boolean adicionarAtividade (Turma turma, Atividade atividade) {
+        if (turma.getAtividade().contains(atividade) == false)
+            return turma.getAtividade().add(atividade);
         return false;
     }
     
     @Override
-    public Boolean removerAtividade (Atividade atividade) {
-        return this.turma.getAtividade().remove(atividade);
+    public Boolean removerAtividade (Turma turma, Atividade atividade) {
+        return turma.getAtividade().remove(atividade);
     }
     
     @Override
-    public Atividade retornaAtividade (Atividade atividade) {
-        return this.turma.getAtividade().get(this.turma.getAtividade().indexOf(atividade));
+    public Atividade retornaAtividade (Turma turma, Atividade atividade) {
+        if (turma.getAtividade().contains(atividade))
+            return turma.getAtividade().get(turma.getAtividade().indexOf(atividade));
+        else
+            return null;
     }
 }
