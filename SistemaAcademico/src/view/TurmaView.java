cdeaf9899acfc168375e4dad09010e8b946fbdc5
JeanPablo <JeanPablo@JeanPablo>
2015-05-10 23:38:40 -0300

    TurmaView

--- SistemaAcademico/src/view/TurmaView.java
+++ SistemaAcademico/src/view/TurmaView.java
@@ -1,10 +1,15 @@
 package view;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import model.dao.AlunoDao;
+import model.dao.AulaDao;
 import model.dao.DisciplinaDao;
 import model.dao.ProfessorDao;
 import model.dao.TurmaDao;
+import model.pojo.Aluno;
+import model.pojo.Aula;
 import model.pojo.Disciplina;
 import model.pojo.Professor;
 import model.pojo.Turma;
@@ -18,9 +23,13 @@ public class TurmaView {
     private TurmaDao turmaDao;
     private DisciplinaDao disciplinaDao;
     private ProfessorDao professorDao;
+    private AulaDao aulaDao;
+    private AlunoDao alunoDao;
+    
     private static Scanner scanner = new Scanner (System.in);
     
     public Boolean cadastrar () {
+        Turma turma = null;
         System.out.println("CADASTRO DE TURMAS\nCadastre uma nova turma:\n");
         System.out.println("ID: ");
         String id = scanner.nextLine();
@@ -36,7 +45,21 @@ public class TurmaView {
         Professor professor = this.obterCadastrado();
         if (professor == null)
             return false;
-        Turma turma = new Turma (id, ano, periodo, numeroDeVagas, disciplina, professor, /*aula*/);
+        
+        System.out.println("***** PARA CONTINUAR, DETERMINE AS AULAS A SEREM ADICIONADAS *****");
+        List<Aula> listaAula = this.obterCadastradas();
+        System.out.println("******************************************************************\n");
+        
+        System.out.println("***** DESEJA ADICIONAR UMA LISTA DE ALUNOS AGORA? *****");
+        System.out.println("Digite ''sim'' para adicionar ou qualquer outro para não: ");
+        if (scanner.nextLine().equals("sim")) {
+            System.out.println("\n");
+            List<Aluno> listaAluno = this.obterCadastrados();
+            turma = new Turma (id, ano, periodo, numeroDeVagas, disciplina, professor,
+                    listaAula, listaAluno);
+        }
+        else
+            turma = new Turma (id, ano, periodo, numeroDeVagas, disciplina, professor, listaAula);
         return this.turmaDao.salvar(turma);
     }
     
@@ -57,6 +80,38 @@ public class TurmaView {
         }
     }
 
+    public List<Aluno> obterCadastrados () {
+        List<Aluno> listaAluno = new ArrayList<>();
+        while (true) {
+            System.out.println("CPF: (digite ''fim'' para terminar: ");
+            String entrada = scanner.nextLine();
+            if (entrada.equals("fim"))
+                break;
+            Aluno aluno = this.alunoDao.obterAluno(entrada);
+            if (aluno != null)
+                listaAluno.add(aluno);
+            else
+                System.out.println("ESTE ALUNO NÃO ESTÁ CADASTRADO!");
+        }
+        return listaAluno;
+    }
+    
+    public List<Aula> obterCadastradas () {
+        List<Aula> listaAula = new ArrayList<>();
+        while (true) {
+            System.out.println("ID Aula (digite ''fim'' para terminar: ");
+            String entrada = scanner.nextLine();
+            if (entrada.equals("fim"))
+                break;
+            Aula aula = this.aulaDao.obterAula(entrada);
+            if (aula != null)
+                listaAula.add(aula);
+            else
+                System.out.println("ESTA AULA NÃO ESTÁ CADASTRADA!");
+        }
+        return listaAula;
+    }
+    
     public Disciplina obterCadastrada () {
         while (true) {
             System.out.println("Disciplina: ");
